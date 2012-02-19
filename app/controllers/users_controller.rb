404eaef96489a27c14e48b0915f5dc488bf558b6
root <root@ip-10-98-91-43.ec2.internal>
2012-02-19 04:13:51 -0500

    Forgot to include password fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
   # GET /users/new.json
   def new
     @user = User.new
-
+    @current_method = "new"
     respond_to do |format|
       format.html # new.html.erb
       format.json { render :json => @user }
