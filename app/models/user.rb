2d3331dc163bc113e00e41f0941d237ad501a0a5
root <root@ip-10-98-91-43.ec2.internal>
2012-02-23 17:27:34 -0500

    Trying to get ldap to work

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,14 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
+  #devise :ldap_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessor :login
+  #attr_accessor :login
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :first_name, :last_name, :phone_number, :login, :role_ids
+  #attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :first_name, :last_name, :phone_number, :login, :role_ids
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :first_name, :last_name, :phone_number, :role_ids
 
   def role?(role)
     return !!self.roles.find_by_name(role.to_s.camelize)
@@ -19,7 +21,7 @@ class User < ActiveRecord::Base
 
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
-    login = conditions.delete(:login)
+    login = conditions.delete(:email)
     where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.strip.downcase }]).first
   end
 
