c2dd3eb21bf3be7eaeb34a5dcabb7aa32e189cd9
root <root@ip-10-98-91-43.ec2.internal>
2012-02-23 21:17:15 -0500

    Explicitly specify full URL in Twixml so that we can have non standard port. The URL is now specifed in config.yml rather than parsing the request uri because it looks like Rails gives the wrong port when running https on non standard https port

--- app/controllers/twilio_call_escalations_controller.rb
+++ app/controllers/twilio_call_escalations_controller.rb
@@ -1,9 +1,9 @@
 class TwilioCallEscalationsController < ApplicationController
   before_filter :force_ssl if AppConfig.use_ssl
-  
+
   # FIXME: should only accept requests from twilio
   def attempt_call
-    @base_url = File.dirname(request.url)
+    @base_url = AppConfig.base_url || ""
     @call_list = CallList.find(params[:call_list_id])
     @caller_id = "+16198003326"
     @call_escalations = @call_list.call_escalations
@@ -17,9 +17,9 @@ class TwilioCallEscalationsController < ApplicationController
     end
   end
   def screen_for_machine
-    @base_url = File.dirname(request.url)
+    @base_url = AppConfig.base_url || ""
   end
   def complete_call
-    @base_url = File.dirname(request.url)
+    @base_url = AppConfig.base_url || ""
   end
 end
