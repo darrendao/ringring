40eb6245f0b76d539bb0b17259f5ac504d021cff
root <root@ip-10-98-91-43.ec2.internal>
2012-03-08 18:27:52 -0500

    Don't try to send sms and phone if number is not available

--- lib/ringring/twilio_helper.rb
+++ lib/ringring/twilio_helper.rb
@@ -2,6 +2,7 @@ module Ringring
 class TwilioHelper
   def self.send_sms(from, to, msg)
     to.each do |to_num|
+      next if to_num.blank?
       $twilio_client.account.sms.messages.create(
         :from => from,
         :to => to_num,
@@ -12,6 +13,7 @@ class TwilioHelper
 
   def self.phone_all(from, to, url)
     to.each do |to_num|
+      next if to_num.blank?
       $twilio_client.account.calls.create(
         :from => from,
         :to => to_num,
