431d13f98223900941ce0551b05f59f8fc4a7512
root <root@ip-10-98-91-43.ec2.internal>
2012-03-21 18:03:20 -0400

    Fix parent child relationship

--- app/models/oncall_assignment.rb
+++ app/models/oncall_assignment.rb
@@ -1,14 +1,6 @@
-class OncallAssignment < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :call_list
-  default_scope :order => 'position'
-  validates :retry, :numericality => {:only_integer => true, :greater_than => 0}, :presence => true
-  validate :user_has_phone_and_sms_info, :check_oncall_datetime
-
-  after_initialize :default_values
-
-#  skip_time_zone_conversion_for_attributes << :starts_at
-#  skip_time_zone_conversion_for_attributes << :ends_at
+class OncallAssignment < CallEscalation
+  set_table_name 'oncall_assignments'
+  validate :check_oncall_datetime
 
   def as_json(options = {})
     {
@@ -31,9 +23,6 @@ class OncallAssignment < ActiveRecord::Base
   end
 
   private
-  def default_values
-    self.retry ||= AppConfig.default_retry || 2
-  end
   def user_has_phone_and_sms_info
     if user.phone_number_info.blank? or user.phone_number_info.number.blank? or user.phone_number_info.sms_gateway.blank?
       errors.add(:phone_number_info, "is missing phone number and/or sms gateway")
