c50e4660d52ab0b420ff61fe6771a822c1433c09
Darren Dao <darrendao@gmail.com>
2012-06-05 12:11:40 -0700

    Add callbacks for when call_list_membership is updated

--- app/models/call_list_membership.rb
+++ app/models/call_list_membership.rb
@@ -2,4 +2,11 @@ class CallListMembership < ActiveRecord::Base
   belongs_to :user
   belongs_to :call_list
   validates :user_id, :uniqueness => {:scope => :call_list_id}
+  after_destroy :cleanup_call_list
+
+  private
+  def cleanup_call_list
+    CallEscalation.where(:call_list_id => call_list_id, :user_id => user_id).destroy_all
+    OncallAssignment.where(:call_list_id => call_list_id, :user_id => user_id).destroy_all
+  end
 end
