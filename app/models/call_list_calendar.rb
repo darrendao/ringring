881dbf32d666ff36fc5848a8225d9f27a37ad4a4
Darren Dao <darrendao@gmail.com>
2012-06-12 12:10:13 -0700

    Minor UI enhancements

--- app/models/call_list_calendar.rb
+++ app/models/call_list_calendar.rb
@@ -1,3 +1,9 @@
 class CallListCalendar < ActiveRecord::Base
   belongs_to :call_list
+  after_initialize :default_values
+
+  private
+  def default_values
+    self.ctype ||= Ringring::Calendar::Type::LOCAL
+  end
 end
