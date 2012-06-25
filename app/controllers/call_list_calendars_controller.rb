d06608b061efa157803dc7104ee7cd88d85b2743
Darren Dao <darrendao@gmail.com>
2012-06-25 16:08:55 -0700

    add hyphen

--- app/controllers/call_list_calendars_controller.rb
+++ app/controllers/call_list_calendars_controller.rb
@@ -8,7 +8,7 @@ class CallListCalendarsController < ApplicationController
       oncall_assignments.each do |oncall_assignment|
         event do
           description "oncall for #{oncall_assignment.call_list.name}"
-          summary "#{oncall_assignment.call_list.name} #{oncall_assignment.user.username}"
+          summary "#{oncall_assignment.call_list.name} - #{oncall_assignment.user.username}"
           dtstart     oncall_assignment.starts_at.to_date
           dtend       oncall_assignment.ends_at.to_date
           uid "#{call_list_calendar.id }-#{oncall_assignment.id}"
